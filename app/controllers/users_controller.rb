7232d78a6204b359ab1b9182e4cab59700cb8bea
xavierlei <xavier.c.f@hotmail.com>
2016-04-23 15:53:19 +0100

    users index with pagination and fake users using the fake gem and seed.rb to populate with fake users run rake db:seed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   #prevent non logged users to edit users info
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   #prevent logged users to edit other's info
   before_action :correct_user,   only: [:edit, :update]
   def show
@@ -33,6 +33,10 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
   end
 
   private
