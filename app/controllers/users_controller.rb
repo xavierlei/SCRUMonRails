b3a4283c57b2f5c29e3e99e7c936d9730ac85a2e
xavierlei <xavier.c.f@hotmail.com>
2016-04-23 18:57:12 +0100

    admin privileges and user delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update]
   #prevent logged users to edit other's info
   before_action :correct_user,   only: [:edit, :update]
+  #to prevent that only admins can delete users
+  before_action :admin_user, only: :destroy
   def show
     @user = User.find(params[:id])
   end
@@ -39,6 +41,12 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page])
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
@@ -56,6 +64,10 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 
 end
