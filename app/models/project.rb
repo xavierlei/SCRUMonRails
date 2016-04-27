62d64b12314db98801910c5ded7941b0c93a5650
xavierlei <xavier.c.f@hotmail.com>
2016-04-27 21:54:57 +0100

    Project render instructions: bundle exec rake db:migrate:reset bundle exec rake db:seed

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,5 @@ class Project < ActiveRecord::Base
   belongs_to :user
   default_scope -> {order(created_at: :desc)}
   validates :user_id, presence: true
-  validates :content, presence: true, length: {maximum: 140}
+  validates :description, presence: true, length: {maximum: 140}
 end
