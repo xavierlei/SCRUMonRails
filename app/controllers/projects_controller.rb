2430745be894453652083d765bad73b364b9939f
xavierlei <xavier.c.f@hotmail.com>
2016-04-29 22:11:00 +0100

    link to new project added

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+  before_action :logged_in_user, only: [:create]
+  before_action :correct_user,   only: [:create]
 
   def new
     @user = User.find(params[:user_id])
@@ -20,6 +22,21 @@ class ProjectsController < ApplicationController
   private
     def project_params
       params.require(:project).permit(:name, :description)
+    end
+ #####This code is (almost) the same that in UsersController.
+ #####move this code to ApplicationController
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:user_id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 
 end
