f1ab5ceccb7c6282c1f5f9911b634ca1f5fb225e
xavierlei <xavier.c.f@hotmail.com>
2016-05-01 11:48:14 +0100

    requirement model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ActiveRecord::Base
   belongs_to :user
+  has_many :requirements, dependent: :destroy
   default_scope -> {order(created_at: :desc)}
   validates :user_id, presence: true
   validates :name, presence: true
