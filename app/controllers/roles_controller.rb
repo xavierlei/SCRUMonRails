56bedc0d7032d47c9db2e070575839682645a486
xavierlei <xavier.c.f@hotmail.com>
2016-05-05 17:49:45 +0100

    authorizations for destroy

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,15 +1,24 @@
 class RolesController < ApplicationController
-  before_action :logged_in_user, only: [:create]
-  before_action :correct_user, only: [:create]
+  before_action :logged_in_user, only: [:create,:destroy]
+  before_action :correct_user, only: [:create,:destroy]
   def new
     @user = User.find(params[:user_id])
     @project = @user.projects.find(params[:project_id])
     @team = @project.teams.find(params[:team_id])
     @role = Role.new
   end
+  def destroy
+    @user = User.find(params[:user_id])
+    @project = @user.projects.find(params[:project_id])
+    @team = @project.teams.find(params[:team_id])
+    if Role.find(params[:id]).destroy
+      flash[:success] = "Team member successfuly removed"
+    else
+      flash[:danger] = @role.errors.full_messages.to_sentence
+    end
+    redirect_to user_project_team_path(@user.id,@project.id,@team.id)
+  end
 
-  #@post = Post.new(:text => params[:post][:text])
-  #Permission.find_by(user_id: params[:user_id], project_id: params[:project_id])
   def create
     @user = User.find(params[:user_id])
     @project = @user.projects.find(params[:project_id])
@@ -20,15 +29,13 @@ class RolesController < ApplicationController
               description: params[:role][:description], email: params[:role][:email])
       if @role.save
         flash[:success] = "member successfuly added"
-        redirect_to user_project_team_path(@user.id,@project.id,@team.id)
       else
-        flash[:danger] = "member adition failed"
-        redirect_to user_project_team_path(@user.id,@project.id,@team.id)
+        flash[:danger] = @role.errors.full_messages.to_sentence
       end
     else
       flash[:danger] = "the user doesn't exist"
-      redirect_to user_project_team_path(@user.id,@project.id,@team.id)
     end
+    redirect_to user_project_team_path(@user.id,@project.id,@team.id)
   end
 
 
