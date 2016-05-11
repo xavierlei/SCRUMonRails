92b55497ed9320fc83a415f8c66cd46e6a113b8d
xavierlei <xavier.c.f@hotmail.com>
2016-05-11 16:21:25 +0100

    scrum board design acceptable to do: add missing crummy

--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -2,7 +2,7 @@ class Requirement < ActiveRecord::Base
   belongs_to :project
   default_scope -> {order(created_at: :desc)}
   validates :project_id, presence: true
-  validates :description, presence: true, length: {maximum: 140}
+  validates :description, presence: true, length: {maximum: 256}
   has_many :tasks, dependent: :destroy
   has_many :teams, through: :tasks
   has_many :sprints, through: :tasks
