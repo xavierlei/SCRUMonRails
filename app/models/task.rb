c4f02eb259e22c12c6ff8d6a8347642f4eeb2a77
xavierlei <xavier.c.f@hotmail.com>
2016-05-11 15:39:03 +0100

    Task model validations ok to do: fix UI design issues on the scrum board

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,8 @@ class Task < ActiveRecord::Base
   belongs_to :team
   belongs_to :sprint
   belongs_to :requirement
-  #validates :team_id, presence: true
-  #validates :sprint_id, presence: true
-  #validates :requirement_id, presence: true
+  validates_uniqueness_of :requirement_id, scope: :sprint_id
+  validates :team_id, presence: true
+  validates :sprint_id, presence: true
+  validates :requirement_id, presence: true
 end
