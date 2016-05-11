4302d173abd5564cc791fd3cbeb0d816d195e176
xavierlei <xavier.c.f@hotmail.com>
2016-05-11 15:26:47 +0100

    Scrum board ok to do: uniqueness validation for task on each sprint design issues

--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -3,6 +3,7 @@ class Requirement < ActiveRecord::Base
   default_scope -> {order(created_at: :desc)}
   validates :project_id, presence: true
   validates :description, presence: true, length: {maximum: 140}
+  has_many :tasks, dependent: :destroy
   has_many :teams, through: :tasks
   has_many :sprints, through: :tasks
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,7 @@ class Team < ActiveRecord::Base
   belongs_to :project
   has_many :roles, dependent: :destroy
   has_many :users, through: :roles
+  has_many :tasks, dependent: :destroy
   has_many :requirements, through: :tasks
   has_many :sprints, through: :tasks
   default_scope ->{order(created_at: :desc)}
